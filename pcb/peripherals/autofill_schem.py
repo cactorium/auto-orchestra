811b608188f2fefd4228ea299f79caa8f70d6c31
Kelvin Ly <kelvin.ly1618@gmail.com>
2018-08-10 23:21:17 -0400

    Save before autofill

---
+++ pcb/peripherals/autofill_schem.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python3
+# TODO: make sure it also works in python2
+
+import argparse
+
+DISTRIBUTOR_VALUES = ["\"Mouser\""]
+
+class Component(object):
+  def __init__(self, start):
+    self.designator = None
+    self.value = None
+    self.footprint = None
+    self.footprint_row = None
+    # distributor ids, mapped to (line_num, value, all fields
+    # (to make reconstruction easier)
+    self.distributors = dict()
+    self.cls = None
+    self.last_value = None
+    self.num_fields = 0
+
+    # used mainly for parser debugging
+    self.start = start
+
+class DistributorData(object):
+  def __init__(self, id):
+    self.id = id
+
+def quotesplit(line):
+  parts = line.split(" ")
+  ret = []
+  num_quotes = 0
+  incomplete_part = None
+  for part in parts:
+    num_quotes += len([None for c in part if c == '"'])
+    if num_quotes % 2 == 1:
+      if incomplete_part is None:
+        # just started a quoted section
+        incomplete_part = part
+      else:
+        incomplete_part += " " + part
+    else:
+      if incomplete_part is not None:
+        # ending quote is in this part
+        ret.append(incomplete_part + " " + part)
+        incomplete_part = None
+      else:
+        # normal case; pass through
+        ret.append(part)
+  return ret
+
+def parse_lines(f):
+  lines = []
+  components = []
+  component_start = False
+  component = None
+
+  for i, line in enumerate(f):
+    if component_start:
+      if line.startswith("L"):
+        parts = quotesplit(line)
+        component.designator = parts[2].rstrip()
+        component.cls = component.designator[0]
+      elif line.startswith("F"):
+        parts = quotesplit(line)
+        component.num_fields += 1
+        component.last_value = i
+        if parts[1].isdigit():
+          field_type = int(parts[1])
+          if field_type == 1:
+            component.value = parts[2]
+          elif field_type == 2:
+            component.footprint_row = (i, parts)
+            if len(parts[2]) > 2:
+              component.footprint = parts[2]
+          elif field_type > 3:
+            # TODO: make case insensitive
+            if parts[-1][:-1] in DISTRIBUTOR_VALUES:
+              component.distributors[parts[-1][:-1]] = DistributorData(parts[2])
+      elif line.startswith("$EndComp"):
+        component_start = False
+        # ignore power nodes
+        if component.cls != "#":
+          components.append(component)
+
+    if line.startswith("$Comp"):
+      component_start = True
+      component = Component(i)
+    lines.append(line)
+  return (lines, components)
+
+def infer_components(components):
+  # dictionaries in dictionaries:
+  # distributor = (distributor_type, id) so that non-unique ids can be captured
+  # filled_values[cls][value] = [(footprint, [designators], [distributors])]
+  filled_values = dict()
+
+  # first pass to infer footprints and distributors, second pass to fill in details
+  for c in components:
+    if c.cls is None or len(c.cls) == 0:
+      print("Component at {} incorrectly parsed; no cls set".format(c.start))
+    else:
+      if c.cls not in filled_values:
+        filled_values[c.cls] = dict()
+
+      if c.value is not None and len(c.value) > 2:
+        if c.value not in filled_values[c.cls]:
+          filled_values[c.cls][c.value] = []
+
+        if c.footprint is not None and len(c.footprint) > 2:
+          # print(c.designator, c.value, c.footprint, c.distributors)
+          tosearch = filled_values[c.cls][c.value]
+          # find element with matching footprint
+          found = None
+          for i, fp in enumerate(tosearch):
+            if fp[0] == c.footprint:
+              found = i
+              break
+
+          if found is None:
+            distributors = []
+            for dist in c.distributors:
+              if len(c.distributors[dist].id) > 2 and len(dist) > 2:
+                distributors.append((dist, c.distributors[dist].id))
+            tosearch.append((c.footprint, [c.designator], distributors))
+          else:
+            tosearch[i][1].append(c.designator)
+            # append any new distributors
+            for dist in c.distributors:
+              if len(c.distributors[dist].id) > 2 and len(dist) > 2:
+                if not any([dist == m[0] and c.distributors[dist].id == m[1] 
+                    for m in tosearch[i][2]]):
+                  tosearch[i][2].append((dist, c.distributors[dist].id))
+  return filled_values
+
+def main():
+  parser = argparse.ArgumentParser(description="Autofills components in an Eeschema schematic")
+  parser.add_argument("input", help="file to autofill")
+  parser.add_argument("output", help="file to write autofilled schematic to")
+
+  args = parser.parse_args()
+
+  lines = None
+  components = None
+
+  with open(args.input, 'r') as f:
+    (lines, components) = parse_lines(f)
+
+  print("{} lines".format(len(lines)))
+  print("found {} components".format(len(components)))
+  without_footprints = len([None for c in components if c.footprint is None])
+  print("found {} components without footprints".format(without_footprints))
+
+  filled_values = infer_components(components)
+
+  entry_count = 0
+  conflicts = []
+  for cls in filled_values:
+    for val in filled_values[cls]:
+      for _ in filled_values[cls][val]:
+        # TODO: check for conflicts
+        entry_count += 1
+  print("found {} filled unique component classes".format(entry_count))
+
+  # TODO: allow interactive distributor choice to resolve conflicts
+  while len(conflicts) > 0:
+    print("found conflicting information, cannot autofill")
+    for conflict in conflicts:
+      pass
+    return
+
+  # autofill
+  autofill_fp = 0
+  autofill_dist = 0
+  to_append = []
+  for c in components:
+    if c.cls in filled_values:
+      if c.value in filled_values[c.cls]:
+        matches = []
+        if c.footprint is not None:
+          matches = [t for t in filled_values[c.cls][c.value] if t[0] == c.footprint]
+        else:
+          matches = filled_values[c.cls][c.value]
+        if len(matches) == 1:
+          # autofill
+          match = matches[0]
+          if ((c.footprint is None or len(c.footprint) <= 2) and 
+              c.footprint_row is not None):
+            print("matched {} {} with {}".format(c.designator, c.value, match))
+            # rewrite footprint
+            c.footprint = match[0]
+            row = c.footprint_row[1]
+            row[2] = match[0]
+            lines[c.footprint_row[0]] = " ".join(row)
+            autofill_fp += 1
+          # add in distributors 
+          dist_added = 0
+          for dist in match[2]:
+            if dist[0] not in c.distributors:
+              c.distributors[dist[0]] = DistributorData(dist[1])
+              # append to the field list
+              template_row = quotesplit(lines[c.last_value][:-1])
+              row = [
+                  template_row[0],
+                  str(c.num_fields),
+                  dist[1]] + template_row[3:11] + [dist[0] + "\n"]
+              c.num_fields += 1
+              to_append.append((c.last_value, row))
+              dist_added += 1
+              # mark as something to do, because appending now would
+              # cause the row numbers to shift for all the components after this one,
+              # invalidating their indices
+          if dist_added > 0:
+            autofill_dist += 1
+
+    # print(c.designator, c.value, c.footprint, c.distributors)
+  for ta in reversed(to_append):
+    idx = ta[0]
+    row = ta[1]
+    lines = lines[0:idx+1] + [" ".join(row)] + lines[idx+1:]
+
+  print("autofilled {} fp, {} dist".format(autofill_fp, autofill_dist))
+  # dictionary of dictionaries of components without footprints
+  # missing[cls][value] = [designators]
+  missing = dict()
+
+  for c in components:
+    if c.cls is not None:
+      if c.cls not in missing:
+        missing[c.cls] = dict()
+      if c.value is not None and c.footprint is None:
+        if c.value not in missing[c.cls]:
+          missing[c.cls][c.value] = []
+        missing[c.cls][c.value].append(c.designator)
+
+  for cls in missing:
+    for value in missing[cls]:
+      print("NOTE: no unique footprint found for {} {}".format(value, missing[cls][value]))
+
+  # repeat for manufacturer info
+  missing = dict()
+
+  for c in components:
+    if c.cls is not None:
+      if c.cls not in missing:
+        missing[c.cls] = dict()
+      if c.value is not None and c.value != "\"DNP\"" and not bool(c.distributors):
+        if c.value not in missing[c.cls]:
+          missing[c.cls][c.value] = []
+        missing[c.cls][c.value].append(c.designator)
+
+  for cls in missing:
+    for value in missing[cls]:
+      print("NOTE: no distributor data found for {} {}".format(value, missing[cls][value]))
+
+
+  output = args.output or args.input
+  print("outputting to {}...".format(output))
+  with open(output, "w+") as f:
+    for line in lines:
+      f.write(line)
+
+if __name__ == "__main__":
+  main()
---
+++ pcb/peripherals/gen_diodes_msop-10.py
@@ -0,0 +1,173 @@
+# dual-row, TSSOP-, SOIC-, SO-style packages with ground plane
+# version 0.0.3
+
+# based on https://www.mouser.com/datasheet/2/115/DGD0506A-1274585.pdf
+# added solder mask parameter
+# added optional indicator circle
+# added ground pad
+# made ground pad optional
+
+PARTNAME = "Diodes-MSOP10"
+
+total_pins = 10
+num_per_edge = total_pins // 2
+
+M1 = 0.2 # silkscreen margin
+M2 = 0.05 # fab outline margin
+M3 = 0.3 # courtyard margin
+
+Z = 5.300 # distance across outer edges of pads
+G = Z - 2*1.35 # distance across inner edges of pads
+
+X = (Z+G)/2. # distance between pad centers
+C = 0.50 # footprint pad spacing
+w = 0.30 # footprint pad width
+h = (Z-G)/2. # footprint pad length
+
+H = 5.0 # distance from physical pad end to opposite pad end
+e = C    # physical pad spacing, same as above
+b = 0.27 # physical pad width
+L1 = (H-3.05)/2 # physical pad length
+
+D1 = 3.05 # package width
+E1 = 3.05 # package height
+
+has_ground_pad = False
+G1 = 2.700  # ground pad width
+H1 = 3.600  # ground pad height
+ground_pad_num = 11
+
+solder_mask_margin = 0.05
+indicator_circle_dia = 0.3
+
+import math
+
+import time
+gen_time = hex(int(time.time()))[2:].upper()
+
+
+prologue = """(module {} (layer F.Cu) (tedit {})
+  (fp_text reference REF** (at 0.0 0.0) (layer F.SilkS)
+    (effects (font (size 1 1) (thickness 0.15)))
+  )
+  (fp_text value {} (at 0 -0.5) (layer F.Fab)
+    (effects (font (size 1 1) (thickness 0.15)))
+  )
+"""
+
+prologue = prologue.format(PARTNAME, gen_time, PARTNAME)
+epilogue = """)"""
+
+print(prologue)
+
+# print silkscreen outline
+inner_edge = C*(num_per_edge/2 - 0.5) + w/2 + M1
+x = D1/2 + M1
+y = E1/2 + M1
+for x, y, nx, ny in [
+    (-x, -inner_edge, -inner_edge, -y),
+    (-inner_edge, -y, x, -y), (x, -y, x, -inner_edge),
+
+    (x, inner_edge, x, y), (x, y, -x, y),
+    (-x, y, -x, inner_edge)]:
+  print("""  (fp_line (start {} {}) (end {} {}) (layer F.SilkS) (width 0.15))""".
+      format(x, y, nx, ny))
+
+if indicator_circle_dia is not None:
+  x = X/2
+  y = D1/2 + M1 + 0.75*indicator_circle_dia
+  # add indicator circle
+  print("""  (fp_circle (center {} {}) (end {} {}) (layer F.SilkS) (width 0.15))""".
+      format(-x, -y - indicator_circle_dia/2., -x, -y))
+
+
+# draw package outline in fab layer
+x, y, nx, ny = -D1/2 - M2, -E1/2 - M2, D1/2 + M2, -E1/2 - M2
+for _ in range(0, 4):
+  print("""  (fp_line (start {} {}) (end {} {}) (layer F.Fab) (width 0.15))""".
+      format(x, y, nx, ny))
+  x, y, nx, ny = nx, ny, -x, -y
+
+for i in range(num_per_edge):
+  x_pos = -D1/2 - M2
+  y_pos = C*(i-(num_per_edge/2-0.5))
+  print("""  (fp_line (start {} {}) (end {} {}) (layer F.Fab) (width 0.15))""".
+      format(x_pos, y_pos - b/2 - M2, x_pos - L1, y_pos - b/2 - M2))
+  print("""  (fp_line (start {} {}) (end {} {}) (layer F.Fab) (width 0.15))""".
+      format(x_pos - L1, y_pos - b/2 - M2, x_pos - L1, y_pos + b/2 + M2))
+  print("""  (fp_line (start {} {}) (end {} {}) (layer F.Fab) (width 0.15))""".
+      format(x_pos - L1, y_pos + b/2 + M2, x_pos, y_pos + b/2 + M2))
+
+for i in range(num_per_edge):
+  x_pos = D1/2 + M2
+  y_pos = -C*(i-(num_per_edge/2-0.5))
+  print("""  (fp_line (start {} {}) (end {} {}) (layer F.Fab) (width 0.15))""".
+      format(x_pos, y_pos + b/2 + M2, x_pos + L1, y_pos + b/2 + M2))
+  print("""  (fp_line (start {} {}) (end {} {}) (layer F.Fab) (width 0.15))""".
+      format(x_pos + L1, y_pos + b/2 + M2, x_pos + L1, y_pos - b/2 - M2))
+  print("""  (fp_line (start {} {}) (end {} {}) (layer F.Fab) (width 0.15))""".
+      format(x_pos + L1, y_pos - b/2 - M2, x_pos, y_pos - b/2 - M2))
+
+
+# print silkscreen outline
+inner_x = D1/2 + M3
+outer_x = X/2 + h/2 + M3/2
+inner_y = C*(num_per_edge/2-0.5) + b/2 + M3/2
+outer_y = E1/2 + M3
+pts = [
+    (-inner_x, -outer_y),
+    (inner_x, -outer_y),
+    (inner_x, -inner_y),
+    (outer_x, -inner_y),
+    (outer_x, inner_y),
+    (inner_x, inner_y),
+    (inner_x, outer_y),
+    (-inner_x, outer_y),
+    (-inner_x, inner_y),
+    (-outer_x, inner_y),
+    (-outer_x, -inner_y),
+    (-inner_x, -inner_y)
+    ]
+nx, ny = pts[-1]
+for px, py in pts:
+  x, y, nx, ny = nx, ny, px, py
+  print("""  (fp_line (start {} {}) (end {} {}) (layer F.CrtYd) (width 0.15))""".
+      format(x, y, nx, ny))
+
+
+for i in range(0, num_per_edge):
+  print("""  (pad {} smd rect (at {} {}) (size {} {}) (layers F.Cu F.Paste F.Mask)
+    (solder_mask_margin {}))""".
+        format(
+            i+1,
+            -X/2,
+            C*(i-(num_per_edge/2-0.5)),
+            h,
+            w,
+            solder_mask_margin))
+for i in range(0, num_per_edge):
+  print("""  (pad {} smd rect (at {} {}) (size {} {}) (layers F.Cu F.Paste F.Mask)
+    (solder_mask_margin {}))""".
+        format(
+            i+1+1*num_per_edge,
+            X/2,
+            -C*(i-(num_per_edge/2-0.5)),
+            h,
+            w,
+            solder_mask_margin))
+
+if has_ground_pad: 
+  # add ground pad
+  print("""  (pad {} smd {} (at {} {}) (size {} {}) (layers F.Cu F.Paste F.Mask)
+    (solder_mask_margin {}))""".
+        format(
+            ground_pad_num,
+            "rect",
+            0,
+            0,
+            G1,
+            H1,
+            solder_mask_margin))
+
+
+print(epilogue)
