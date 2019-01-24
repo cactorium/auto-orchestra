cc430c098d67fd68b357adc47c664990757db1f5
Kelvin Ly <kelvin.ly1618@gmail.com>
2019-01-24 18:53:44 -0500

    Split USB code into separate file, working timer

--- fw/receiver-adc/src/main.c
+++ fw/receiver-adc/src/main.c
@@ -18,232 +18,52 @@
  */
 
 #include <stdlib.h>
+
+#include <libopencm3/cm3/nvic.h>
+
 #include <libopencm3/stm32/crs.h>
-#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/syscfg.h>
-#include <libopencm3/usb/usbd.h>
-#include <libopencm3/usb/cdc.h>
+#include <libopencm3/stm32/timer.h>
 
-static const struct usb_device_descriptor dev = {
-  .bLength = USB_DT_DEVICE_SIZE,
-  .bDescriptorType = USB_DT_DEVICE,
-  .bcdUSB = 0x0200,
-  .bDeviceClass = USB_CLASS_CDC,
-  .bDeviceSubClass = 0,
-  .bDeviceProtocol = 0,
-  .bMaxPacketSize0 = 64,
-  .idVendor = 0x0483,
-  .idProduct = 0x5740,
-  .bcdDevice = 0x0200,
-  .iManufacturer = 1,
-  .iProduct = 2,
-  .iSerialNumber = 3,
-  .bNumConfigurations = 1,
-};
+#include "usb.c"
 
-/*
- * This notification endpoint isn't implemented. According to CDC spec its
- * optional, but its absence causes a NULL pointer dereference in Linux
- * cdc_acm driver.
- */
-static const struct usb_endpoint_descriptor comm_endp[] = {{
-  .bLength = USB_DT_ENDPOINT_SIZE,
-    .bDescriptorType = USB_DT_ENDPOINT,
-    .bEndpointAddress = 0x83,
-    .bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
-    .wMaxPacketSize = 64,
-    .bInterval = 255,
-}};
+static void timer3_setup() {
+  rcc_periph_clock_enable(RCC_TIM3);
+  nvic_enable_irq(NVIC_TIM3_IRQ);
 
-static const struct usb_endpoint_descriptor data_endp[] = {{
-  .bLength = USB_DT_ENDPOINT_SIZE,
-    .bDescriptorType = USB_DT_ENDPOINT,
-    .bEndpointAddress = 0x01,
-    .bmAttributes = USB_ENDPOINT_ATTR_BULK,
-    .wMaxPacketSize = 64,
-    .bInterval = 1,
-}, {
-  .bLength = USB_DT_ENDPOINT_SIZE,
-    .bDescriptorType = USB_DT_ENDPOINT,
-    .bEndpointAddress = 0x82,
-    .bmAttributes = USB_ENDPOINT_ATTR_BULK,
-    .wMaxPacketSize = 64,
-    .bInterval = 1,
-}};
+  //timer_reset(TIM3);
+  timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+  // prescaled clock = 100 kHz, 10 us
+  timer_set_prescaler(TIM3, 480);
+  // slow down to 10 Hz for now
+  timer_set_period(TIM3, 10000);
+  timer_disable_oc_output(TIM3, TIM_OC1 | TIM_OC2 | TIM_OC3 | TIM_OC4);
+  timer_disable_oc_clear(TIM3, TIM_OC1);
+  timer_disable_oc_preload(TIM3, TIM_OC1);
+  timer_set_oc_slow_mode(TIM3, TIM_OC1);
+  timer_set_oc_mode(TIM3, TIM_OC1, TIM_OCM_FROZEN);
+  timer_set_oc_value(TIM3, TIM_OC1, 0);
 
-static const struct {
-  struct usb_cdc_header_descriptor header;
-  struct usb_cdc_call_management_descriptor call_mgmt;
-  struct usb_cdc_acm_descriptor acm;
-  struct usb_cdc_union_descriptor cdc_union;
-} __attribute__((packed)) cdcacm_functional_descriptors = {
-  .header = {
-    .bFunctionLength = sizeof(struct usb_cdc_header_descriptor),
-    .bDescriptorType = CS_INTERFACE,
-    .bDescriptorSubtype = USB_CDC_TYPE_HEADER,
-    .bcdCDC = 0x0110,
-  },
-  .call_mgmt = {
-    .bFunctionLength =
-      sizeof(struct usb_cdc_call_management_descriptor),
-    .bDescriptorType = CS_INTERFACE,
-    .bDescriptorSubtype = USB_CDC_TYPE_CALL_MANAGEMENT,
-    .bmCapabilities = 0,
-    .bDataInterface = 1,
-  },
-  .acm = {
-    .bFunctionLength = sizeof(struct usb_cdc_acm_descriptor),
-    .bDescriptorType = CS_INTERFACE,
-    .bDescriptorSubtype = USB_CDC_TYPE_ACM,
-    .bmCapabilities = 0,
-  },
-  .cdc_union = {
-    .bFunctionLength = sizeof(struct usb_cdc_union_descriptor),
-    .bDescriptorType = CS_INTERFACE,
-    .bDescriptorSubtype = USB_CDC_TYPE_UNION,
-    .bControlInterface = 0,
-    .bSubordinateInterface0 = 1,
-  },
-};
+  timer_continuous_mode(TIM3);
+  timer_disable_preload(TIM3);
+  timer_enable_counter(TIM3);
+  timer_enable_irq(TIM3, TIM_DIER_CC1IE);
+}
 
-static const struct usb_interface_descriptor comm_iface[] = {{
-  .bLength = USB_DT_INTERFACE_SIZE,
-    .bDescriptorType = USB_DT_INTERFACE,
-    .bInterfaceNumber = 0,
-    .bAlternateSetting = 0,
-    .bNumEndpoints = 1,
-    .bInterfaceClass = USB_CLASS_CDC,
-    .bInterfaceSubClass = USB_CDC_SUBCLASS_ACM,
-    .bInterfaceProtocol = USB_CDC_PROTOCOL_AT,
-    .iInterface = 0,
+usbd_device *usbd_dev;
 
-    .endpoint = comm_endp,
+void tim3_isr() {
+  if (timer_get_flag(TIM3, TIM_SR_CC1IF)) {
+    timer_clear_flag(TIM3, TIM_SR_CC1IF);
 
-    .extra = &cdcacm_functional_descriptors,
-    .extralen = sizeof(cdcacm_functional_descriptors),
-}};
-
-static const struct usb_interface_descriptor data_iface[] = {{
-  .bLength = USB_DT_INTERFACE_SIZE,
-    .bDescriptorType = USB_DT_INTERFACE,
-    .bInterfaceNumber = 1,
-    .bAlternateSetting = 0,
-    .bNumEndpoints = 2,
-    .bInterfaceClass = USB_CLASS_DATA,
-    .bInterfaceSubClass = 0,
-    .bInterfaceProtocol = 0,
-    .iInterface = 0,
-
-    .endpoint = data_endp,
-}};
-
-static const struct usb_interface ifaces[] = {{
-  .num_altsetting = 1,
-    .altsetting = comm_iface,
-}, {
-  .num_altsetting = 1,
-    .altsetting = data_iface,
-}};
-
-static const struct usb_config_descriptor config = {
-  .bLength = USB_DT_CONFIGURATION_SIZE,
-  .bDescriptorType = USB_DT_CONFIGURATION,
-  .wTotalLength = 0,
-  .bNumInterfaces = 2,
-  .bConfigurationValue = 1,
-  .iConfiguration = 0,
-  .bmAttributes = 0x80,
-  .bMaxPower = 0x32,
-
-  .interface = ifaces,
-};
-
-static const char *usb_strings[] = {
-  "Black Sphere Technologies",
-  "CDC-ACM Demo",
-  "DEMO",
-};
-
-/* Buffer to be used for control requests. */
-uint8_t usbd_control_buffer[128];
-
-static enum usbd_request_return_codes cdcacm_control_request(usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf,
-    uint16_t *len, void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req)) {
-  (void)complete;
-  (void)buf;
-  (void)usbd_dev;
-
-  switch (req->bRequest) {
-    case USB_CDC_REQ_SET_CONTROL_LINE_STATE: {
-                                               /*
-                                                * This Linux cdc_acm driver requires this to be implemented
-                                                * even though it's optional in the CDC spec, and we don't
-                                                * advertise it in the ACM functional descriptor.
-                                                */
-                                               char local_buf[10];
-                                               struct usb_cdc_notification *notif = (void *)local_buf;
-
-                                               /* We echo signals back to host as notification. */
-                                               notif->bmRequestType = 0xA1;
-                                               notif->bNotification = USB_CDC_NOTIFY_SERIAL_STATE;
-                                               notif->wValue = 0;
-                                               notif->wIndex = 0;
-                                               notif->wLength = 2;
-                                               local_buf[8] = req->wValue & 3;
-                                               local_buf[9] = 0;
-                                               // usbd_ep_write_packet(0x83, buf, 10);
-                                               return USBD_REQ_HANDLED;
-                                             }
-    case USB_CDC_REQ_SET_LINE_CODING:
-                                             if (*len < sizeof(struct usb_cdc_line_coding))
-                                               return USBD_REQ_NOTSUPP;
-                                             return USBD_REQ_HANDLED;
+    static char buf[] = "test\r\n";
+    usbd_ep_write_packet(usbd_dev, 0x82, buf, sizeof(buf)-1);
   }
-  return USBD_REQ_NOTSUPP;
-}
-
-static void cdcacm_data_rx_cb(usbd_device *usbd_dev, uint8_t ep) {
-  (void)ep;
-  (void)usbd_dev;
-
-  char buf[64];
-  int len = usbd_ep_read_packet(usbd_dev, 0x01, buf, 64);
-
-  if (len) {
-    usbd_ep_write_packet(usbd_dev, 0x82, buf, len);
-    buf[len] = 0;
-  }
-}
-
-static void cdcacm_set_config(usbd_device *usbd_dev, uint16_t wValue) {
-  (void)wValue;
-  (void)usbd_dev;
-
-  usbd_ep_setup(usbd_dev, 0x01, USB_ENDPOINT_ATTR_BULK, 64, cdcacm_data_rx_cb);
-  usbd_ep_setup(usbd_dev, 0x82, USB_ENDPOINT_ATTR_BULK, 64, NULL);
-  usbd_ep_setup(usbd_dev, 0x83, USB_ENDPOINT_ATTR_INTERRUPT, 64, NULL);
-
-  usbd_register_control_callback(
-      usbd_dev,
-      USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE,
-      USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
-      cdcacm_control_request);
-}
-
-
-static void usb_setup(void) {
-  /* Enable clocks for GPIO port A and USB peripheral. */
-  rcc_periph_clock_enable(RCC_USB);
-  rcc_periph_clock_enable(RCC_GPIOA);
-  rcc_periph_clock_enable(RCC_SYSCFG_COMP);
-
-  rcc_set_usbclk_source(RCC_PLL);
-  SYSCFG_CFGR1 |= (1 << 4);
 }
 
 int main(void) {
-  usbd_device *usbd_dev;
 
   /*
      rcc_clock_setup_in_hsi48_out_48mhz();
@@ -252,6 +72,7 @@ int main(void) {
      */
   rcc_clock_setup_in_hse_8mhz_out_48mhz();
 
+  timer3_setup();
   usb_setup();
 
   usbd_dev = usbd_init(&st_usbfs_v2_usb_driver, &dev, &config, usb_strings,
